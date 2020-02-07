effdf77c1f2642a7d67cbabf9e8db6839adb93ed
jiun-dev <58166091+jiun-dev@users.noreply.github.com>
2020-02-07 16:40:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-a = 221
-b = a - 5;
-console.log(b)
+const calculator = {
+    plus:function(a,b){
+        return a + b;
+    },
+    minus:function(a,b){
+        return a - b;
+    },
+    multi:function(a,b){
+        return a * b;
+    },
+    div:function(a,b){
+        return a / b;
+    },
+    exponentiation:function(a,b){
+        return a ** b;
+    },
+}
+
+//  console.log(greetNicolas)
+const plus = calculator.plus(5, 5)
+const minus = calculator.minus(10, 5)
+const multi = calculator.multi(10, 5)
+const div = calculator.div(10, 5)
+const exponentiation = calculator.exponentiation(10, 5)
+console.log(plus)
+console.log(minus)
+console.log(multi)
+console.log(exponentiation)
